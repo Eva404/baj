75ab674c1fad743c004105d3c17d512980da4a8b
Eva404 <75418692+Eva404@users.noreply.github.com>
2021-01-01 23:13:40 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,55 @@
+green='\033[1;92m'
+yellow='\033[1;93m'
+white='\033[1;97m'
+cyan='\033[1;96m'
+echo "$yellow
+=================================
+Created By Mr:James
+================================="
+echo ""
+echo ""
+echo "$green
+====================================
+Installing termux Parrot shell
+===================================="
+echo "$yellow
+================================
+Installing packages
+================================"
+apt update
+apt install ruby -y
+gem install lolcat
+echo "green
+======================
+Done
+======================"
+echo ""
+echo ""
+echo "
+======================
+Initializing...
+======================"
+rm $PREFIX/etc/bash.bashrc
+cp bash.bashrc $PREFIX/etc
+echo "green
+======================
+Done
+======================"
+clear
+echo "$yellow
+=================================
+Created By Mr:James
+================================="
+echo "$yellow
+Instagram : Mr:James
+================================="
+echo "$yellow
+github.com/James404-cyber
+================================="
+echo ""
+echo "$green
+======================================
+Now type 'exit' & restart Termux
+======================================"
+echo ""
+echo "$white"
